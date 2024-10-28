5936527e759a94178a45a47388258eabf27a9a03
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-10-28 22:35:05 +0900

    Feat read command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+use dotenvy::dotenv;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[clap(short = 'p', long = "port", env, default_value = "3000")]
+    port: u16,
+    #[clap(short = 'c', long = "cert", env)]
+    cert_path: String,
+    #[clap(short = 'k', long = "private", env)]
+    private_key_path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _ = dotenv();
+
+    let args = Args::parse();
+
+    println!("{:?}", args);
 }
