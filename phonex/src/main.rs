d51866de410642be1c625c912757797da8647e3b
Masataka Hisasue <masataka.hisasue@gmail.com>
2025-01-02 19:05:33 +0900

    Feat single websocket

--- phonex/src/main.rs
+++ phonex/src/main.rs
@@ -1,11 +1,10 @@
 mod err;
 
 use err::PhonexError;
-use futures_util::stream::FuturesUnordered;
 use futures_util::{SinkExt, StreamExt};
 use signal;
 use std::ops::ControlFlow;
-use std::sync::{Arc, Weak};
+use std::sync::Arc;
 use std::time::Instant;
 use tokio_tungstenite::tungstenite::Utf8Bytes;
 use tracing_spanned::SpanErr;
@@ -74,10 +73,8 @@ fn on_ice_candidate() -> Result<OnLocalCandidateHdlrFn, SpanErr<PhonexError>> {
 #[tokio::main]
 async fn main() -> Result<(), SpanErr<PhonexError>> {
     let start_time = Instant::now();
-    //spawn several clients that will concurrently talk to the server
-    let mut clients = (0..N_CLIENTS)
-        .map(|cli| tokio::spawn(spawn_client(cli)))
-        .collect::<FuturesUnordered<_>>();
+
+    let websocket_task = tokio::spawn(spawn_client());
 
     let peer_connection = initialize_peer_connection().await?;
 
@@ -95,16 +92,11 @@ async fn main() -> Result<(), SpanErr<PhonexError>> {
 
     // TODO send offer
 
-    //wait for all our clients to exit
-    while clients.next().await.is_some() {}
+    let _ = websocket_task.await;
 
     let end_time = Instant::now();
 
-    //total time should be the same no matter how many clients we spawn
-    println!(
-        "Total time taken {:#?} with {N_CLIENTS} concurrent clients, should be about 6.45 seconds.",
-        end_time - start_time
-    );
+    println!("Total time taken {:#?}", end_time - start_time);
 
     peer_connection.close().await.unwrap();
 
@@ -112,17 +104,15 @@ async fn main() -> Result<(), SpanErr<PhonexError>> {
 }
 
 //creates a client. quietly exits on failure.
-async fn spawn_client(who: usize) {
+async fn spawn_client() {
     let ws_stream = match connect_async(SERVER).await {
         Ok((stream, response)) => {
-            println!("Handshake for client {who} has been completed");
-            // This will be the HTTP response, same as with server this is the last moment we
-            // can still access HTTP stuff.
+            println!("Handshake has been completed");
             println!("Server response was {response:?}");
             stream
         }
         Err(e) => {
-            println!("WebSocket handshake for client {who} failed with {e}!");
+            println!("WebSocket handshake failed with {e}!");
             return;
         }
     };
@@ -159,7 +149,6 @@ async fn spawn_client(who: usize) {
         tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
 
         // When we are done we may want our client to close connection cleanly.
-        println!("Sending close to {who}...");
         if let Err(e) = sender
             .send(Message::Close(Some(CloseFrame {
                 code: CloseCode::Normal,
@@ -175,7 +164,7 @@ async fn spawn_client(who: usize) {
     let mut recv_task = tokio::spawn(async move {
         while let Some(Ok(msg)) = receiver.next().await {
             // print message and break if instructed to do so
-            if process_message(msg, who).is_break() {
+            if process_message(msg).is_break() {
                 break;
             }
         }
@@ -234,34 +223,34 @@ fn candidate_message(target_id: String, candidate: String) -> Result<String, ser
     return serde_json::to_string(&message);
 }
 
-fn process_message(msg: Message, who: usize) -> ControlFlow<(), ()> {
+fn process_message(msg: Message) -> ControlFlow<(), ()> {
     match msg {
         Message::Text(t) => {
-            println!(">>> {who} got str: {t:?}");
+            println!(">>> got str: {t:?}");
         }
         Message::Binary(d) => {
-            println!(">>> {} got {} bytes: {:?}", who, d.len(), d);
+            println!(">>> got {} bytes: {:?}", d.len(), d);
         }
         Message::Close(c) => {
             if let Some(cf) = c {
                 println!(
-                    ">>> {} got close with code {} and reason `{}`",
-                    who, cf.code, cf.reason
+                    ">>> got close with code {} and reason `{}`",
+                    cf.code, cf.reason
                 );
             } else {
-                println!(">>> {who} somehow got close message without CloseFrame");
+                println!(">>> got close message without CloseFrame");
             }
             return ControlFlow::Break(());
         }
 
         Message::Pong(v) => {
-            println!(">>> {who} got pong with {v:?}");
+            println!(">>> got pong with {v:?}");
         }
         // Just as with axum server, the underlying tungstenite websocket library
         // will handle Ping for you automagically by replying with Pong and copying the
         // v according to spec. But if you need the contents of the pings you can see them here.
         Message::Ping(v) => {
-            println!(">>> {who} got ping with {v:?}");
+            println!(">>> got ping with {v:?}");
         }
 
         Message::Frame(_) => {
